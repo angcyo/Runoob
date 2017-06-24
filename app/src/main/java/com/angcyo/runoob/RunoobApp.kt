dce4f389bc6b25492173a922bef3acf74cd4d04e
angcyo <angcyo@angcyo-mac-pro.lan>
2017-06-24 21:08:47 +0800

    init

---
+++ app/src/main/java/com/angcyo/runoob/RunoobApp.kt
@@ -0,0 +1,14 @@
+package com.angcyo.runoob
+
+import com.angcyo.uiview.RApplication
+import com.angcyo.uiview.Root
+
+/**
+ * Created by angcyo on 2017-06-24.
+ */
+class RunoobApp : RApplication() {
+    override fun onInit() {
+        super.onInit()
+        Root.APP_FOLDER = "Runoob"
+    }
+}
