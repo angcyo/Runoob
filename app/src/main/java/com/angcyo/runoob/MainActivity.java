3b18a0a648b1ca11c34d00653b2ce8c1cc6280d2
angcyo <angcyo@angcyo-mac-pro.lan>
2017-06-24 21:04:07 +0800

    init

---
+++ app/src/main/java/com/angcyo/runoob/MainActivity.java
@@ -0,0 +1,13 @@
+package com.angcyo.runoob;
+
+import android.content.Intent;
+
+import com.angcyo.uiview.base.UILayoutActivity;
+
+public class MainActivity extends UILayoutActivity {
+
+    @Override
+    protected void onLoadView(Intent intent) {
+
+    }
+}
